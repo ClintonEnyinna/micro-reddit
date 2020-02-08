0313ba51f8db32b1294f3ae9e909d3891e266ce6
ClintonEnyinna <enyinnaclinton@gmail.com>
2020-02-07 18:28:12 -0600

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: { message: "must be given please" }, length: { maximum: 20 }, uniqueness: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :username, presence: { message: 'must be given please' }, length: { maximum: 20 }, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
 
